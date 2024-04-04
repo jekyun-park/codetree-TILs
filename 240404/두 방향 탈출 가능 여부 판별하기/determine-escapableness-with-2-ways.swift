4f55b4d77934c533f3d7d711fd02556ca1e901e3
Jegyun <parkjekyun@gmail.com>
2024-04-04 21:03:31 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 20ms, Memory: 1MB, Status: Runtime Error - CodeTree

--- 240404/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.swift
+++ 240404/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.swift
@@ -31,7 +31,7 @@ func dfs(_ x: Int, _ y: Int) {
 }
 
 func isInRange(_ x: Int, _ y: Int) -> Bool {
-    return (0..<n ~= x) && (0..<m ~= y)
+    return ((0..<n) ~= x) && ((0..<m) ~= y)
 }
 
 func canMoveTo(_ x: Int, _ y: Int) -> Bool {

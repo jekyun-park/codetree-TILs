99b21970ffd2ae29e32f024943802ed2508693bb
Jegyun <parkjekyun@gmail.com>
2024-04-05 16:45:19 +0900

    Title: 안전 지대, Time: 39ms, Memory: 1MB, Status: Passed - CodeTree

--- 240405/안전 지대/comfort-zone.swift
+++ 240405/안전 지대/comfort-zone.swift
@@ -1,32 +1,39 @@
 import Foundation
 
-// O(N * M * K): 50 * 50 * 100 = 250,000 
-// 1억회(100,000,000) 연산 = 1초
-
-let nm = readLine()!.split(separator: " ").map { Int($0)! }
-let n = nm[0], m = nm[1]
+let nm = readLine()!.split(separator: " ").map { Int($0)! } 
 var originalMap = [[Int]]()
 
-for _ in 0..<n {
-    originalMap.append(readLine()!.split(separator: " ").map { Int($0)! })
+for _ in 0..<nm[0] {
+    let input = readLine()!.split(separator: " ").map { Int($0)! } 
+    originalMap.append(input)
 }
 
-var answers: [(K: Int, count: Int)] = []
-var visited = Array(repeating: Array(repeating: false, count: m), count: n)
+let maxHeight = originalMap.flatMap{ $0 }.max()!
+var visited = Array(repeating: Array(repeating: false, count: nm[1]), count: nm[0])
+var results = [(k: Int, safeAreaCount: Int)]()
+var count = 1
 var map = [[Int]]()
 
-for k in 1...100 {
-    map = originalMap.map { $0.map { $0 - k } }
-    for i in 0..<n {
-        for j in 0..<m {
-            let r = dfs(i, j, 0)
-            print(r)
+for k in 1...maxHeight {
+    map = originalMap.map{ $0.map{ $0 - k } }
+    var safeArea = [Int]()
+    
+    for i in 0..<nm[0] {
+        for j in 0..<nm[1] {
+            if map[i][j] > 0 {
+                count = 1
+                dfs(i, j)
+                safeArea.append(count) 
+            }
         }
     }
-    visited = Array(repeating: Array(repeating: false, count: m), count: n)
+
+    results.append((k, safeArea.count)) 
+    safeArea.removeAll()
+    visited = Array(repeating: Array(repeating: false, count: nm[1]), count: nm[0])
 }
 
-func dfs(_ x: Int, _ y: Int, _ c: Int) -> Int {
+func dfs(_ x: Int, _ y: Int) {
     let dx = [-1, 1, 0, 0]
     let dy = [0, 0, -1, 1]
 
@@ -36,21 +43,26 @@ func dfs(_ x: Int, _ y: Int, _ c: Int) -> Int {
         let nx = x + dx[i]
         let ny = y + dy[i]
 
-        if canMove(nx, ny) {
+        if isSafeArea(nx, ny) {
             visited[nx][ny] = true
-            dfs(nx, ny, c+1)
+            map[nx][ny] = 0
+            count += 1
+            dfs(nx, ny)
         }
     }
-
-    return c
 }
 
-func isInRange(_ x: Int, _ y: Int) -> Bool {
-    return ((0..<n) ~= x) && ((0..<m) ~= y)
-}
-
-func canMove(_ x: Int, _ y: Int) -> Bool {
-    if !isInRange(x, y) { return false }
-    if map[x][y] < 1 { return false }
+func isSafeArea(_ x: Int, _ y: Int) -> Bool {
+    if !((0..<nm[0] ~= x) && (0..<nm[1] ~= y)) { return false }
+    if (visited[x][y] || map[x][y] <= 0) { return false }
     return true
 }
+
+results.sort {
+    if $0.safeAreaCount == $1.safeAreaCount {
+        return $0.k < $1.k
+    }
+    return $0.safeAreaCount > $1.safeAreaCount
+}
+
+print(results.first!.k, results.first!.safeAreaCount)

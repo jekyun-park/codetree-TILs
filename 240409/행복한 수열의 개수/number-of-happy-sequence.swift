860c2508b8dc307dfad0b565a2076f4905845da2
Jegyun <parkjekyun@gmail.com>
2024-04-09 20:23:05 +0900

    Title: 행복한 수열의 개수, Time: 10ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240409/행복한 수열의 개수/number-of-happy-sequence.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nm[0], m = nm[1]
+var grid = [[Int]]()
+var answer = 0
+
+for _ in 0..<n {
+    grid.append(readLine()!.split(separator: " ").map { Int($0)! })
+}
+
+for row in grid {
+    var last = 0
+    var count = 1
+    for i in row {
+        if last == 0 {
+            last = i
+        } else if last == i {
+            count += 1 
+        } else {
+            last = i
+            count = 1
+        }
+    }
+    if count >= m { answer += 1 }
+}
+
+for col in 0..<n {
+    var last = 0
+    var count = 1
+    for i in 0..<n {
+        if last == 0 {
+            last = grid[col][i]
+        } else if last == grid[col][i] {
+            count += 1 
+        } else {
+            last = grid[col][i]
+            count = 1
+        }
+    }
+    if count >= m { answer += 1 }
+}
+
+
+print(answer)

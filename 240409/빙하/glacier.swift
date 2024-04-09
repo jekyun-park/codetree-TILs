469d23d05a7541f0834318c706e0b0b8f89a80e4
Jegyun <parkjekyun@gmail.com>
2024-04-09 20:13:08 +0900

    Title: 빙하, Time: 1097ms, Memory: 1MB, Status: Time Limit Exceed - CodeTree

--- 240409/빙하/glacier.swift
+++ 240409/빙하/glacier.swift
@@ -5,48 +5,55 @@ let n = nm[0], m = nm[1]
 var grid = [[Int]]()
 var visited = Array(repeating: Array(repeating: false, count: m), count: n)
 var queue = Queue<(Int, Int)>()
-var water = Set<[Int]>()
-var answer = 0
+var glacierCount = 0
 var time = 0
+let dy = [-1, 1, 0, 0], dx = [0, 0, -1, 1]
 
 for _ in 0..<n {
     grid.append(readLine()!.split(separator: " ").map { Int($0)! })
 }
 
-
 while true {
-    if grid.flatMap{ $0 }.filter{ $0 == 1 }.count == 0 {
-        break
-    }
-    bfs(0, 0)
-    answer = meltGlacier(water)
-    visited = Array(repeating: Array(repeating: false, count: m), count: n)
-    water.removeAll()
-    time += 1
+    simulate()
+    if !isGlacierExist() { break }
 }
 
-print(time, answer)
+print(time, glacierCount)
 
-func meltGlacier(_ water: Set<[Int]>) -> Int {
-    let dy = [-1, 1, 0, 0]
-    let dx = [0, 0, -1, 1]
-    var count = 0
+func isGlacierExist() -> Bool {
+    return grid.flatMap { $0 }.filter { $0 == 1 }.count > 0
+}
 
-    for point in water {
-        for i in 0..<4 {
-            let ny = point[0] + dy[i]
-            let nx = point[1] + dx[i]
+func simulate() {
+    time += 1
+    glacierCount = 0
+    bfs()
+    meltGlacier()
+}
 
-            if  isInRange(ny, nx) {
-                if grid[ny][nx] == 1 {
-                    grid[ny][nx] = 0
-                    count += 1
-                }
+func meltGlacier() {
+    for i in 0..<n {
+        for j in 0..<m {
+            if grid[i][j] == 1 && check(i, j) {
+                grid[i][j] = 0
+                glacierCount += 1
             }
         }
     }
+}
 
-    return count
+func check(_ y: Int, _ x: Int) -> Bool {
+    for i in 0..<4 {
+        let ny = y + dy[i], nx = x + dx[i]
+        if isInRange(ny, nx) && visited[ny][nx] {
+            return true
+        }
+    }
+    return false
+}
+
+func isGlacier(_ y: Int, _ x: Int) -> Bool {
+    return isInRange(y, x) && (grid[y][x] == 1) && !visited[y][x]
 }
 
 func isInRange(_ y: Int, _ x: Int) -> Bool {
@@ -59,37 +66,22 @@ func canMove(_ y: Int, _ x: Int) -> Bool {
     return true
 }
 
-
-
-func bfs(_ y: Int, _ x: Int) {
-
-    let dy = [-1, 1, 0, 0]
-    let dx = [0, 0, -1, 1]
-
-    visited[y][x] = true
-    water.insert([y, x])
-    queue.enqueue((y, x))
-
+func bfs() {
+    visited[0][0] = true
+    queue.enqueue((0, 0))
 
     while !queue.isEmpty {
-
         guard let (y, x) = queue.dequeue() else { return }
-
         for i in 0..<4 {
             let ny = y + dy[i]
             let nx = x + dx[i]
 
             if canMove(ny, nx) {
                 visited[ny][nx] = true
-                water.insert([ny, nx])
-
                 queue.enqueue((ny, nx))
             }
         }
     }
-
-    
-    
 }
 
 

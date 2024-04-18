e1534f78e6ebde06013bd2c32f1ad6700370c4b1
Jegyun <parkjekyun@gmail.com>
2024-04-18 22:05:26 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240418/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.swift
+++ 240418/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.swift
@@ -43,12 +43,12 @@ while !queue.isEmpty {
     } else if (number % 2 == 0) && !visited.contains(number/2){
         visited.insert(number/2)
         queue.enqueue((count+1, number/2))
-    } else if !visited.contains(number+1) {
-        visited.insert(number+1)
-        queue.enqueue((count+1, number+1))
     } else if !visited.contains(number-1) {
         visited.insert(number-1)
         queue.enqueue((count+1, number-1))
+    } else if !visited.contains(number+1){
+        visited.insert(number+1)
+        queue.enqueue((count+1, number+1))
     }
 }
 

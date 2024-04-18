93a14b1c5cdd2ef93b0a265983eea974033318e8
Jegyun <parkjekyun@gmail.com>
2024-04-18 20:43:34 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 10ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240418/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+struct Queue<T> {
+    private var inbox: [T] = []
+    private var outbox: [T] = []
+
+    var isEmpty: Bool {
+        return inbox.isEmpty && outbox.isEmpty
+    }
+
+    mutating func enqueue(_ element: T) {
+        inbox.append(element)
+    }
+
+    mutating func dequeue() -> T? {
+        if outbox.isEmpty {
+            outbox = inbox.reversed()
+            inbox.removeAll()
+        }
+        return outbox.popLast()
+    }
+}
+
+var N = Int(readLine()!)!
+var answer = 0
+
+while N != 1 {
+    if N % 3 == 0 {
+        N /= 3
+    } else if N % 2 == 0 {
+        N /= 2
+    } else {
+        N += 1
+    }
+    answer += 1
+}
+
+print(answer)
